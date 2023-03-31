851a9c781218235638eb0da29d3fbb017f7b8996
Nurrahman <m360dkx3793@bangkit.academy>
2023-04-01 04:34:14 +0700

    New

---
+++ Code/Arduino_Mega_Master/Arduino_Mega_Master.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  Serial2.begin(9600);
+  Serial3.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    if (command == '1') {             
+      Serial.println("Pump On");
+      Serial3.write("1"); // send "1" as a string
+    }
+    else if (command == '0') { 
+      Serial.println("Pump Off");
+      Serial3.write("0"); // send "0" as a string     
+    }
+  }
+}
