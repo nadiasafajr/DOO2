22a631f667ae283710eb0c7524cd8d1e64b99723
Nurrahman <m360dkx3793@bangkit.academy>
2023-04-03 03:52:17 +0700

    Update

--- Code/Arduino_Mega_Master/Arduino_Mega_Master.ino
+++ Code/Arduino_Mega_Master/Arduino_Mega_Master.ino
@@ -11,23 +11,24 @@ void loop() {
   if (Serial.available() > 0) {
     char command = Serial.read();
     if (command == '1') {             
-      Serial.println("Pump On");
-      Serial3.println('1'); // send "1" as a string
+      Serial3.print('1'); // send "1" as a string
     }
     else if (command == '0') { 
-      Serial.println("Pump Off");
-      Serial3.println('0'); // send "0" as a string     
+      Serial3.print('0'); // send "0" as a string     
     }
   }
 
-if (Serial2.available()>0){
-  String data = Serial2.readStringUntil('\n');
-  int separatorIndex = data.indexOf(',');
-  int tinggi = data.substring(11, separatorIndex).toInt();
-  int literPermenit = data.substring(separatorIndex + 5).toInt();
-
-Serial.print("Waterlevel: ");
-Serial.println(tinggi);
-Serial.print(" Flow: ");
-Serial.println(literPermenit);  }
+if (Serial2.available() > 0) {
+    String data = Serial2.readStringUntil('\n');
+    int separatorIndex = data.indexOf(',');
+    if (separatorIndex != -1) {
+      int tinggi = data.substring(11, separatorIndex).toInt();
+      int literPermenit = data.substring(separatorIndex + 1).toInt();
+      Serial.print("Water level: ");
+      Serial.println(tinggi);
+      Serial.print("Flow: ");
+      Serial.println(literPermenit);
+    }
+  }
+delay(300);
 }
