95c8b089f9632382f82e01382735bb4604ed4e87
gitnurrahman <m360dkx3793@bangkit.academy>
2023-04-05 10:19:50 +0700

    update

--- Code/Arduino_Mega_Master/Arduino_Mega_Master.ino
+++ Code/Arduino_Mega_Master/Arduino_Mega_Master.ino
@@ -7,8 +7,8 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (Serial.available()) {
+  // put your main code here, to run   repeatedly:
+  if (Serial.available ()>0) {
     // read user input from computer
     char command = Serial.read();
 
@@ -16,24 +16,22 @@ void loop() {
     Serial3.print(command);
   }
 
-if (Serial2.available()) {
-    //int tinggi = Serial2.parseInt();
-    //Serial.print("Distance: ");
-    //Serial.print(tinggi);
-
-    // read and print flow rate data from flow sensor on slave
-    //int literPermenit = Serial2.parseInt();
-    //Serial.print("Flow rate: ");
-    //Serial.println(literPermenit);
-
-    String data = Serial2.readStringUntil('\n');
+if (Serial2.available()>0) {
+    String data = Serial.readStringUntil('\n');
     
-    // Pisahkan data menjadi nilai jarak dan aliran
-    int separatorIndex = data.indexOf(',');
-    int tinggi; //= data.substring(1,separatorIndex).toInt();
-    int literPermenit = data.substring(separatorIndex + 1).toInt();
-    Serial.print(tinggi);
+    // Split data into water level, flow rate and PID values
+    int separatorIndex1 = data.indexOf(',');
+    int separatorIndex2 = data.indexOf(',', separatorIndex1 + 1);
+    int tinggi = data.substring(12, separatorIndex1).toInt();
+    int literPermenit = data.substring(separatorIndex1 + 8, separatorIndex2).toInt();
+    int output_PID = data.substring(separatorIndex2 + 6).toInt();
+    
+    // Print water level, flow rate and PID values
+    Serial.print("Water level: ");
+    Serial.println(tinggi);
+    Serial.print("Flow rate: ");
     Serial.println(literPermenit);
-
+    Serial.print("PID: ");
+    Serial.println(output_PID);
   }
 }
